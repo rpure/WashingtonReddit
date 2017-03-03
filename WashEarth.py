cc166ba280a5662e52a354c00af845aba412611a
Ubuntu <ubuntu@ip-172-31-30-94.us-west-2.compute.internal>
2017-03-03 19:45:54 +0000

    changed logging to only printing for now, submissions work correctly

--- WashEarth.py
+++ WashEarth.py
@@ -2,7 +2,7 @@
 import praw
 import logging
 
-logging.basicConfig(filename='WashEarthError.log',level=logging.DEBUG)
+#logging.basicConfig(filename='WashEarthError.log',level=logging.DEBUG)
 
 reddit = praw.Reddit('WashingtonEPBot')
 subreddit = reddit.subreddit("EarthPorn")
@@ -13,18 +13,15 @@ f.close()
 
 for submission in subreddit.stream.submissions():
     normalized_title = submission.title.lower()
+    #print(normalized_title)
     for keyword in washington_keywords:
         try:
             if keyword in normalized_title:
                 post_title = str.join('', ('\"', str(submission.title), '\"', ', posted by u/', str(submission.author)))
-                reddit.subreddit('WashingtonEarthPorn').submit(url=submission.url, title=post_title)
+		reddit.subreddit('WashingtonEarthPorn').submit(url=submission.url, title=post_title)
                 break
-        except:
-            logging.warning("ERROR: ")
-            logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-            logging.warning('is when this event was logged.')
-            logging.warning(normalized_title)
-            logging.warning('is what caused the error')
-            e = sys.exc_info()[0]
-            logging.warning(e)
-
+        except Error as e:
+            print("ERROR")
+            print(submission.title)
+            print(e)
+            break
