9c4bc19ae46a9be9c7b38bf2a95a0133dad478ef
Ubuntu <ubuntu@ip-172-31-30-94.us-west-2.compute.internal>
2017-03-04 00:56:51 +0000

    Fixed unicode error when printing submission

--- WashEarth.py
+++ WashEarth.py
@@ -13,16 +13,15 @@ f.close()
 
 for submission in subreddit.stream.submissions():
     normalized_title = submission.title.lower()
-    #print(normalized_title)
     for keyword in washington_keywords:
         try:
             if keyword in normalized_title:
-                post_title = str.join('', ('\"', str(submission.title), '\"', ', posted by u/', str(submission.author)))
+                post_title = str.join('', ('\"', repr(submission.title), '\"', ', posted by u/', repr(submission.author)))
 		print('Found ' + keyword + ' in ' + normalized_title)
 		reddit.subreddit('WashingtonEarthPorn').submit(url=submission.url, title=post_title)
                 break
         except Exception as e:
-            print("ERROR")
-            print(submission.title)
+	    print("ERROR")
+            print(repr(submission.title))
             print(e)
             break
