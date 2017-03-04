a0b25181589a518c4fb878873b51a14399d5bf47
Ubuntu <ubuntu@ip-172-31-30-94.us-west-2.compute.internal>
2017-03-04 02:13:16 +0000

    Actually fixed encoding error, now using Python 3

--- WashEarth.py
+++ WashEarth.py
@@ -2,26 +2,27 @@
 import praw
 import logging
 
-#logging.basicConfig(filename='WashEarthError.log',level=logging.DEBUG)
-
 reddit = praw.Reddit('WashingtonEPBot')
-subreddit = reddit.subreddit("EarthPorn")
+EPSubreddit = reddit.subreddit("EarthPorn")
+WashEPSubreddit = reddit.subreddit("WashingtonEarthPorn")
+keywordsFile = "WashingtonKeyWords.txt"
 
-with open("WashingtonKeyWords.txt") as f:
+with open(keywordsFile) as f:
     washington_keywords = f.read().splitlines();
 f.close()
 
-for submission in subreddit.stream.submissions():
+for submission in EPSubreddit.stream.submissions():
     normalized_title = submission.title.lower()
     for keyword in washington_keywords:
         try:
             if keyword in normalized_title:
-                post_title = str.join('', ('\"', repr(submission.title), '\"', ', posted by u/', repr(submission.author)))
-		print('Found ' + keyword + ' in ' + normalized_title)
-		reddit.subreddit('WashingtonEarthPorn').submit(url=submission.url, title=post_title)
+                title = str(submission.title)
+                user = str(submission.author)
+                post_title = str.join('', ('\"', title, '\"', ', posted by u/', user))
+                WashEPSubreddit.submit(url=submission.url, title=post_title)
+                print("\nPOSTED\n\t" + post_title + "\n")
                 break
         except Exception as e:
-	    print("ERROR")
-            print(repr(submission.title))
-            print(e)
+            print("\nERROR\n\t" + str(submission.title))
+            print("\t" + str(e) + "\n")
             break
